62f62e27e9301cfdb0b8767d6ea1d2425e4946d0
fremi <microsys.net@gmail.com>
2022-01-10 00:16:52 -0600

    carrito

---
+++ Curso JS Moderno/15-PROYECTO-Carrito/js/app.js
@@ -0,0 +1,10 @@
+/** variable */
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-cursos tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+cargarEventListeners();
+function cargarEventListeners() {
+
+}
